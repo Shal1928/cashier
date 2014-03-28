8a27bd729d4a2a8b4588db24915af7ea4efb857b
Shal1928 <Shal1928@yandex.ru>
2014-03-28 20:36:19 +0800

    Добавил стили для интерфейса кассира, общая разметка сетки

--- src/main/java/it/q02/asocp/modules/cashier/client/ui/layout/CashierView.java
+++ src/main/java/it/q02/asocp/modules/cashier/client/ui/layout/CashierView.java
@@ -1,29 +1,44 @@
 package it.q02.asocp.modules.cashier.client.ui.layout;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
+import it.q02.asocp.modules.cashier.client.ui.resources.Resources;
 import org.gwtbootstrap3.client.ui.FluidContainer;
 
 /**
  *
  */
 public class CashierView implements IsWidget {
+    //region UiBinder base
     interface CashierViewUiBinder extends UiBinder<FluidContainer, CashierView> {
     }
 
     private static CashierViewUiBinder ourUiBinder = GWT.create(CashierViewUiBinder.class);
+    //endregion
 
-    private FluidContainer rootElement = ourUiBinder.createAndBindUi(this);
+
+    //region UiFields
+    private FluidContainer rootElement;
+
+    @UiField(provided = true)
+    protected Resources res;
+    //endregion
+
 
     public CashierView() {
-
+        res = GWT.create(Resources.class);
+        res.layoutStyle().ensureInjected();
+        rootElement = ourUiBinder.createAndBindUi(this);
     }
 
+
+    //region IsWidget
     @Override
     public Widget asWidget() {
         return rootElement;
     }
+    //endregion
 }
---
+++ src/main/java/it/q02/asocp/modules/cashier/client/ui/resources/Resources.java
@@ -0,0 +1,13 @@
+package it.q02.asocp.modules.cashier.client.ui.resources;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import it.q02.asocp.modules.cashier.client.ui.resources.css.LayoutStyle;
+
+/**
+ *
+ */
+public interface Resources extends ClientBundle {
+    @Source("css/LayoutStyle")
+    LayoutStyle layoutStyle();
+}
