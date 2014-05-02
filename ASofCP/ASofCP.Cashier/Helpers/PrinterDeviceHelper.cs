9470358304531aa7b7a565f51790b60cf4e13533
Shal1928 <Shal1928@yandex.ru>
2014-05-03 01:45:47 +0800

    1.1.1.1 Проверка принтера
    Изменена проверка принтера, теперь происходит запрос статуса. Возвращет
    false в трех случаях: Offline (7), Error (9), Not Aviable (11)

--- ASofCP/ASofCP.Cashier/Helpers/PrinterDeviceHelper.cs
+++ ASofCP/ASofCP.Cashier/Helpers/PrinterDeviceHelper.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Linq;
-using System.Management;
+﻿using System.Management;
+using log4net;
 
 namespace ASofCP.Cashier.Helpers
 {
     public static class PrinterDeviceHelper
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(PrinterDeviceHelper));
+
+        public static string[] ExtendedPrinterStatus = { 
+            "","Other", "Unknown", "Idle", "Printing", "Warming Up",
+            "Stopped Printing", "Offline", "Paused", "Error", "Busy",
+            "Not Available", "Waiting", "Processing", "Initialization",
+            "Power Save", "Pending Deletion", "I/O Active", "Manual Feed"
+        };
+
+        public static string[] ErrorState = {
+            "Unknown", "Other", "No Error", "Low Paper", "No Paper", "Low Toner",
+            "No Toner", "Door Open", "Jammed", "Offline", "Service Requested",
+            "Output Bin Full"
+        };
+
+        //public static bool IsPlug(string printerName)
+        //{
+        //    if (printerName.IsNullOrEmptyOrSpaces()) return false;
+
+        //    var scope = new ManagementScope(@"\root\cimv2");
+        //    scope.Connect();
+
+        //    var allPrinters = new ManagementObjectSearcher("SELECT * FROM Win32_Printer");
+        //    return (from ManagementBaseObject printer in allPrinters.Get() 
+        //            let name = printer.GetPropertyValue("Name").ToString() 
+        //            where String.Equals(printerName, name, StringComparison.InvariantCultureIgnoreCase) 
+        //            select (bool) printer.GetPropertyValue("WorkOffline")).FirstOrDefault();
+        //}
+
         public static bool IsPlug(string printerName)
         {
             if (printerName.IsNullOrEmptyOrSpaces()) return false;
@@ -13,11 +41,18 @@ namespace ASofCP.Cashier.Helpers
             var scope = new ManagementScope(@"\root\cimv2");
             scope.Connect();
 
-            var allPrinters = new ManagementObjectSearcher("SELECT * FROM Win32_Printer");
-            return (from ManagementBaseObject printer in allPrinters.Get() 
-                    let name = printer.GetPropertyValue("Name").ToString() 
-                    where String.Equals(printerName, name, StringComparison.InvariantCultureIgnoreCase) 
-                    select (bool) printer.GetPropertyValue("WorkOffline")).FirstOrDefault();
+            var query = new ObjectQuery("SELECT ExtendedPrinterStatus FROM Win32_Printer WHERE Name =\"{0}\"".F(printerName));
+            var searcher = new ManagementObjectSearcher(scope, query);
+            var collection = searcher.Get();
+
+            foreach (var obj in collection)
+            {
+                var status = (uint)obj["ExtendedPrinterStatus"];
+                Log.Debug("Статус принтера {0}", ExtendedPrinterStatus[status]);
+                if (status == 7 || status == 9 || status == 11) return false;
+            }
+
+            return true;
         }
     }
 }
